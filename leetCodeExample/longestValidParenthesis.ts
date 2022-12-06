29f8c43d106c57d181b866ac7a2e01bd88ccf83e
Ranger Chenore <r2chenore@gmail.com>
2022-12-05 19:59:15 -0500

    Add more examples

---
+++ leetCodeExample/longestValidParenthesis.ts
@@ -0,0 +1,7 @@
+/*
+Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses 
+substring
+
+https://leetcode.com/problems/longest-valid-parentheses/
+
+*/
---
+++ phoneNumberRegex/phoneNumberRegex.ts
@@ -0,0 +1,1 @@
+// Regex for matching a phone number
